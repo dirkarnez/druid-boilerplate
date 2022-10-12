c3bd3a933e6aca3309f5baa4cc54dee389ac0b35
dirkarnez <smalldirkalex@gmail.com>
2022-10-12 21:51:20 +0800

    - init, wip

--- local-build.cmd
+++ local-build.cmd
@@ -1,21 +1,35 @@
 @echo off
 
-set USERPROFILE=D:\Softwares\rust-portable
+set DOWNLOADS_DIR=%USERPROFILE%\Downloads
+
+set USERPROFILE=%DOWNLOADS_DIR%\rust-portable
 set CARGO_HOME=%USERPROFILE%\.cargo
-set RUST_PATH=D:\Softwares\rust-portable\.cargo\bin
+set RUST_PATH=%DOWNLOADS_DIR%\rust-portable\.cargo\bin
 set RUSTUP_HOME=%USERPROFILE%\.rustup
 
 set PATH=^
-D:\Softwares\rust-portable\.cargo\bin;^
-D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
-D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;
+%DOWNLOADS_DIR%\rust-portable\.cargo\bin;^
+%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+%DOWNLOADS_DIR%\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;
+
+
+@REM set USERPROFILE=D:\Softwares\rust-portable
+@REM set CARGO_HOME=%USERPROFILE%\.cargo
+@REM set RUST_PATH=D:\Softwares\rust-portable\.cargo\bin
+@REM set RUSTUP_HOME=%USERPROFILE%\.rustup
+
+@REM set PATH=^
+@REM D:\Softwares\rust-portable\.cargo\bin;^
+@REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64;^
+@REM D:\Softwares\x86_64-8.1.0-release-posix-seh-rt_v6-rev0\mingw64\bin;
 
 
 rustup set default-host x86_64-pc-windows-gnu
 @REM rustup install stable
 @REM rustup default stable
 
+cd src
 @REM 
 @REM cargo install --path .
-cargo build
+cargo build --release
 pause
